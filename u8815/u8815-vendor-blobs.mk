2d4b366bb1b3f390756dba398a3d171698fe0185
ezeteze <785462@gmail.com>
2013-07-12 15:11:20 +0200

    Only 2030 baseband.
    Change-Id: Icbbcddcc35d6a5ce2bfc9e5ee032aee8bdf66690

--- u8815/u8815-vendor-blobs.mk
+++ u8815/u8815-vendor-blobs.mk
@@ -30,8 +30,7 @@ PRODUCT_COPY_FILES += \
     vendor/huawei/u8815/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/huawei/u8815/proprietary/etc/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
     vendor/huawei/u8815/proprietary/etc/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
-    vendor/huawei/u8815/proprietary/lib/109808/libcm.so:system/lib/109808/libcm.so \
-    vendor/huawei/u8815/proprietary/lib/2030/libcm.so:system/lib/2030/libcm.so \
+    vendor/huawei/u8815/proprietary/lib/2030/libcm.so:system/lib/libcm.so \
     vendor/huawei/u8815/proprietary/lib/egl/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
     vendor/huawei/u8815/proprietary/lib/egl/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/huawei/u8815/proprietary/lib/egl/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
